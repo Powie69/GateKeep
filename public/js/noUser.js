b9b15448b9c9ab745908a0900d36a1d2feb835ce
Powie <PowieIsCool@gmail.com>
2024-07-15 18:36:11 +0800

    add noUser.html

---
+++ public/js/noUser.js
@@ -0,0 +1,30 @@
+async function loginSubmit(event) {
+    event.preventDefault();
+	const data = Object.fromEntries(new FormData(event.target).entries());
+	if (!(/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(data.username) || /^09\d{9}$/.test(data.username)) || !/^[1-6]\d{11}$/.test(data.lrn) || !data.password) {
+		console.log("Invalid login data (client)");
+	}
+    console.log("Valid login data (client)");
+    try {
+        const response = await fetch('profile/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            body: new URLSearchParams(data),
+			credentials: 'include',
+        });
+
+		const respond = await response.json();
+
+        if (!response.ok) {
+			document.querySelector('.main-form-message').innerText = respond.message;
+			document.querySelector('.main-form-message').classList.remove("_noDisplay");
+			return;
+        } else {
+			document.querySelector('.main-form-message').classList.add("_noDisplay");
+			location.reload();
+			return;
+		}
+    } catch (error) {console.error(error);}
+}
